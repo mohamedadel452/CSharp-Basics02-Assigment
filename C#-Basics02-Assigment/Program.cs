674ebef3e69c329d8f758193ccd7fcfbf24a7ef3
Mohamed Adel <m7711843@gmail.com>
2026-09-06 14:08:34 +0300

    Q1 is answerd

--- C#-Basics02-Assigment/Program.cs
+++ C#-Basics02-Assigment/Program.cs
@@ -12,6 +12,25 @@ namespace BookAssigment
 
             // Class of Book created with the title and  pages
 
+            #region Q1 (Creating an instance of the Book class And store it on a variable of type Object And Print it and its properties)
+
+
+            Console.WriteLine("Q1 (Creating an instance of the Book class And store it on a variable of type Object)");
+
+            Object book1 = new Book("Kotlin In Action", 500);
+
+            // Print the object And it will print the name of the class 
+            Console.WriteLine($"Printing the book object: {book1}");
+
+            //  Print the title and pages of the book 
+           
+            Console.WriteLine("Printing the title and pages of the book");
+            Console.WriteLine($"The title of the book is: {((Book)book1).Title}");
+            Console.WriteLine($"The number of pages in the book is: {((Book)book1).Pages}");
+
+            #endregion
+
+
 
 
 
