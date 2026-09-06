3423d9263a4420995f7785bc06d8fde3b6f165f8
SOUQ <SOUQ@DESKTOP-GPIDMDK>
2026-09-06 13:00:52 +0300

    Add project files.

---
+++ C#-Basics02-Assigment/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// This is a simple C# program that demonstrates the use of classes and methods
+namespace BookAssigment
+{
+    // This is the main class of the program
+    class Program
+    {
+
+        // Entry point of the program
+        static void Main(string[] args)
+        {
+       
+           
+        }
+    }
+
+
+}
+
